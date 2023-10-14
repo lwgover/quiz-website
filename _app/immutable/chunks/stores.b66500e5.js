258b50f9e470eaefe95fb0e6dd931f65ddcae992
lwgover <lgover@pugetsound.edu>
2023-10-13 23:03:38 -0400

    Updates

--- _app/immutable/chunks/stores.b66500e5.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./singletons.1b69a6ff.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
